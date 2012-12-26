7a51e8e8312fec895a34d109f9f2e9f7ba5cd366
Anton <deil@mneko.net>
2012-12-26 23:48:59 +0400

    updated to use new version of Cirrocumulus gem

--- balancer.rb
+++ balancer.rb
@@ -2,6 +2,10 @@ require 'bundler/setup'
 require 'cirrocumulus'
 require_relative 'vm_balancer_ontology'
 
+JabberChannel::server('89.223.109.2')
+JabberChannel::conference('cirrocumulus')
+JabberChannel::jid_suffix('172.16.11.4')
+
 agent = Cirrocumulus::Environment.new(`hostname`.chomp)
 agent.load_ontology(VmBalancerOntology.new(Agent.network('balancer')))
 agent.run

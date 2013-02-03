bebb95f8a44a3d9873e88748c494aba4d13e4f48
Anton <deil@mneko.net>
2013-02-04 00:41:39 +0400

    wip

--- vm_balancer_ontology.rb
+++ vm_balancer_ontology.rb
@@ -30,6 +30,7 @@ class VmBalancerOntology < Ontology
 
   rule 'got_fresh_statistics', [ Statistics.new(:is => :fresh) ] do |ontology, params|
     ontology.logger.info "Statistics is fresh. Working"
+    ontology.process_statistics
   end
 
   rule 'obsolete_statistics', [ Statistics.new(:is => :fresh) ], :for => 120.seconds do |ontology, params|
@@ -43,6 +44,31 @@ class VmBalancerOntology < Ontology
   def refresh_statistics
     replace [:statistics, :is, :STATE], :refreshing
     create_saga(RefreshStatsSaga).start()
+  end
+
+  def process_statistics
+    matcher = PatternMatcher.new(@facts.enumerate)
+    matches = matcher.find_matches_for_condition([:guest, :GUEST_ID, :cpu_time, :CPU_TIME]).map {|data| data.data}
+    matches.each do |fact|
+      guest_id = fact[1]
+      cpu_time = fact[3]
+
+      vif_stats = matcher.find_matches_for_condition([:guest, guest_id, :vif, :VIF, :rx, :RX, :tx, :TX]).map {|data| data.data}
+      vif_stats.each do |vif|
+        vif_num = vif[3]
+        rx_bytes = vif[5]
+        tx_bytes = vif[7]
+      end
+
+      block_stats = matcher.find_matches_for_condition([:guest, guest_id, :block_device, :DEV, :reads, :RD_REQ, :rd_bytes, :RD_BYTES, :writes, :WR_REQ, :wr_bytes, :WR_BYTES]).map {|data| data.data}
+      block_stats.each do |block|
+        block_device = block[3]
+        num_reads = block[5]
+        bytes_read = block[7]
+        num_writes = block[9]
+        bytes_written = block[11]
+      end
+    end
   end
 
   def logger

4843ab9a422f98d83f6da50739cd3f8aab08bb8c
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2013-05-25 10:54:22 +0400

    Build migration map and handle migration errors

--- Scheduler/src/balance.py
+++ Scheduler/src/balance.py
@@ -30,14 +30,23 @@ def optimum():
 # List of triples (vm_id, ps_from_id, ps_to_id) TODO make Action class
 # Build using optimum
 def build_migration_map():
-    return []
+    result = []
+    for server_info in optimum():
+        server = objects.find_ps(server_info[0])
+        for vm_id in server_info[1]:
+            if not server.find_vm(vm_id):
+                result.append(())
+        for vm_id in server.vm_list:
+            if not vm_id in server_info[1]:
+                result.append(())
+    return result
 
 
 ## Logically implement load balancing ##
 def balance(migration_map):
     for action in migration_map:
         if not migrate.migrate(action[0], action[1], action[2]):
-            print "PANIC!"
+            print "Migration error %i from %i to %i" % (action[0], action[1], action[2])
 
 
 ## Periodically check and update ##
